b2f9a5bea637e6cade64bebc5e4eb1004ae30729
alicelee1231 <saemmilee1231@gmail.com>
2024-04-23 11:13:15 +0900

    set사용, state 사용 방법 공부

--- applereact/src/App.js
+++ applereact/src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import './App.css';
 import {useState} from 'react';
 
@@ -5,8 +7,21 @@ function App() {
   let post = 'ff'; 
 
   // a는 값 저장, b는 state 변경을 도와주는 함수
-  let [logo, setLogo] = useState(['ReactBlog','second','third']);
+  let [logo, setLogo] = useState(['ReactBlog', 'third', 'second']);
+  let [ like, setLike] = useState(0);
+  let [change, setChange] = useState('gender');
 
+
+  // function cun(){
+  //   if(like === 'hello'){
+  //     setLike('nice to meet u');
+  //   }else {
+  //     setLike('hello');
+  //   }
+  // }
+
+
+  
   return (
     <div className="App">
       {/* className으로 css file 가져오기 */}
@@ -17,13 +32,28 @@ function App() {
     {/* 변수는 {} */}
       <h4>{post}</h4>
       <div className = "list"> 
-        <h4> {logo[0]}</h4>
+        <h4> {logo[0]} <span onClick={() => {setLike(like + 1)}}>👍</span> {like} </h4>
         <p>4월 22일 발행</p>
       </div>
+
+    <button onClick={() => {
+      let copy1 = [...logo];
+      copy1.sort();
+      setLogo(copy1);
+    }}>가나다라</button>
+
+    {/* state는 기존의 값과 변경할 값을 먼저 비교해서 바꿈, 예로 밑에 setLogo(copy)를 기존의 내용과 비교해서 변경*/}
+    {/* array, object는 deep copy, shallo copy로 하기 */}
       <div className = "list"> 
-        <h4> {logo[1]}</h4>
+        <h4> {logo[1]} <button onClick = {() => {
+          let copy = [...logo];
+          copy[0] = 'java';
+          setLogo(copy);
+        }}>글 수정 </button> </h4>
         <p>4월 23일 발행</p>
       </div>
+
+
       <div className = "list"> 
         <h4> {logo[2]}</h4>
         <p>4월 24일 발행</p>
