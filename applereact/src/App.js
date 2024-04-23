448d3584965363746a5c0ed376c2e1a2d02d21a2
alicelee1231 <saemmilee1231@gmail.com>
2024-04-23 16:16:02 +0900

    modal안보이게

--- applereact/src/App.js
+++ applereact/src/App.js
@@ -1,65 +1,71 @@
-/* eslint-disable */
+/*eslint-disable */
 
 import './App.css';
-import {useState} from 'react';
+import { useState } from 'react';
+
 
 function App() {
-  let post = 'ff'; 
+  let post = 'ff';
 
-  // a는 값 저장, b는 state 변경을 도와주는 함수
   let [logo, setLogo] = useState(['ReactBlog', 'third', 'second']);
-  let [ like, setLike] = useState(0);
-  let [change, setChange] = useState('gender');
+  let [like, setLike] = useState(0);
+  let [modal, setModal] = useState(0);
+  let [titlec, setTitleC] = useState(0);
 
 
-  // function cun(){
-  //   if(like === 'hello'){
-  //     setLike('nice to meet u');
-  //   }else {
-  //     setLike('hello');
-  //   }
-  // }
-
-
-  
   return (
     <div className="App">
-      {/* className으로 css file 가져오기 */}
       <div className="black-nav">
-        {/* style은 object형식으로 가져오기 */}
-        <h4 style = {{color: "red", fontSize : '100px'} }>{post}</h4>
+        <h4 style={{ color: "red", fontSize: '100px' }} >{post}</h4>
       </div>
-    {/* 변수는 {} */}
+
+    
       <h4>{post}</h4>
-      <div className = "list"> 
-        <h4> {logo[0]} <span onClick={() => {setLike(like + 1)}}>👍</span> {like} </h4>
+      <div className="list">
+        <h4> {logo[0]} <span onClick={() => { setLike(like + 1) }}>👍</span> {like} </h4>
         <p>4월 22일 발행</p>
       </div>
 
-    <button onClick={() => {
-      let copy1 = [...logo];
-      copy1.sort();
-      setLogo(copy1);
-    }}>가나다라</button>
+      <button onClick={() => {
+        let copy1 = [...logo];
+        copy1.sort();
+        setLogo(copy1);
+      }}>가나다라</button>
 
-    {/* state는 기존의 값과 변경할 값을 먼저 비교해서 바꿈, 예로 밑에 setLogo(copy)를 기존의 내용과 비교해서 변경*/}
-    {/* array, object는 deep copy, shallo copy로 하기 */}
-      <div className = "list"> 
-        <h4> {logo[1]} <button onClick = {() => {
+      <div className="list">
+        <h4> {logo[1]} <button onClick={() => {
           let copy = [...logo];
           copy[0] = 'java';
           setLogo(copy);
         }}>글 수정 </button> </h4>
         <p>4월 23일 발행</p>
       </div>
-
-
-      <div className = "list"> 
+      <div className="list">
         <h4> {logo[2]}</h4>
         <p>4월 24일 발행</p>
       </div>
+
+      <div className="list">
+        <h4> <button onClick={() => { setModal(1) }}>여기</button></h4>
+      </div>
+      {
+        modal == true ? <Modal/> : null
+      }
     </div>
   );
 }
+
+function Modal() {
+  return (
+    <div className='modal'>
+      <h4>title</h4>
+      <p>date</p>
+      <p>content</p>
+    </div>
+  );
+}
+
+
+
 
 export default App;
