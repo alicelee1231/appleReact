69c4715e64b9a0a7fc215cb0c59a41849a01c9f0
alicelee1231 <saemmilee1231@gmail.com>
2024-04-24 16:15:50 +0900

    state 공부

--- applereact/src/App.js
+++ applereact/src/App.js
@@ -8,9 +8,9 @@ function App() {
   let post = 'ff';
 
   let [logo, setLogo] = useState(['ReactBlog', 'third', 'second']);
-  let [like, setLike] = useState(0);
+  let [like, setLike] = useState([0,0,0]);
   let [modal, setModal] = useState(0);
-  let [titlec, setTitleC] = useState(0);
+  let [cnt, setCnt] = useState(1);
 
 
   return (
@@ -19,7 +19,7 @@ function App() {
         <h4 style={{ color: "red", fontSize: '100px' }} >{post}</h4>
       </div>
 
-    
+{/*     
       <h4>{post}</h4>
       <div className="list">
         <h4> {logo[0]} <span onClick={() => { setLike(like + 1) }}>👍</span> {like} </h4>
@@ -30,37 +30,55 @@ function App() {
         let copy1 = [...logo];
         copy1.sort();
         setLogo(copy1);
-      }}>가나다라</button>
+      }}>가나다라</button> */}
 
-      <div className="list">
-        <h4> {logo[1]} <button onClick={() => {
-          let copy = [...logo];
-          copy[0] = 'java';
-          setLogo(copy);
-        }}>글 수정 </button> </h4>
-        <p>4월 23일 발행</p>
-      </div>
-      <div className="list">
-        <h4> {logo[2]}</h4>
-        <p>4월 24일 발행</p>
-      </div>
+        {
+          // function 파라미터 부분은 map이 돌아가면서 array의 요소들을 하나씩 꺼내오는 것
+          // fuction 부분에는 두개의 파라미터가 들어감. 앞의 파라미터는 array의 순서대로 가져오는 거고
+          // 뒤의 파라미터는 1씩 더해지는 정수임 그래서 h4부분에 a를 logo[i]로해도 i부분이 1씩 더해지기 때문에 array부분이 하나씩 나옴
+          //map앞의 array 개수만큼 map이 돌아감 -> return 문 안에 있는 내용을 array에 담아줌 ex) [<div>hey</div>] -> 이런 형식을 react가 알아서 배열 벗겨서 실행하는거
+          logo.map(function(a,i){
+            return (<div className="list">
+            <h4 > {a} <button onClick={() => {
+              let copy = [...logo];
+              copy[0] = 'java';
+              setLogo(copy);
+            }}>글 수정 </button> </h4>
+            <h4> <button onClick={() => {setModal(1); setCnt(i)}}>여기</button></h4>
+            <h4> <span onClick={() => { 
+                let copy = [...like];
+                copy[i] = copy[i] + 1;
+                setLike(copy);
+            }}>👍</span> {like[i]} </h4>
+            <p>4월 23일 발행</p>
+          </div>)
+         
+          })
+        }
+
+        {/* <button onClick={() => {setCnt(0)}}>글제목0</button>
+        <button onClick={() => {setCnt(1)}}>글제목1</button>
+        <button onClick={() => {setCnt(2)}}>글제목2</button> */}
+
 
       <div className="list">
         <h4> <button onClick={() => { setModal(1) }}>여기</button></h4>
       </div>
+      {/* props의 이름은 가져오는  요소와 이름을 동일하게함. 왜? 작명하기 귀찮아서 */}
       {
-        modal == true ? <Modal/> : null
+        modal == true ? <Modal color={'skyblue'} logo={logo} cnt={cnt}/> : null
       }
     </div>
   );
 }
 
-function Modal() {
+function Modal(props) {
   return (
-    <div className='modal'>
-      <h4>title</h4>
+    <div className='modal' style={{background : props.color}}>
+      <h4>{props.logo[props.cnt]}</h4>
       <p>date</p>
       <p>content</p>
+      <button>push</button>
     </div>
   );
 }
