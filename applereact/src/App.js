c4192d477774807077241c53ad6569a4d047a2ab
alicelee1231 <saemmilee1231@gmail.com>
2024-04-25 13:25:34 +0900

    삭제, 수정 버튼

--- applereact/src/App.js
+++ applereact/src/App.js
@@ -5,20 +5,47 @@ import { useState } from 'react';
 
 
 function App() {
-  let post = 'ff';
+  let [post,setPost] = useState(['ff']);
 
   let [logo, setLogo] = useState(['ReactBlog', 'third', 'second']);
   let [like, setLike] = useState([0,0,0]);
   let [modal, setModal] = useState(0);
   let [cnt, setCnt] = useState(1);
+  let [input, setInput] = useState('');
 
+  const addd = () => {
+    setPost(() => [input]);
+  }
 
   return (
+    <>
     <div className="App">
       <div className="black-nav">
         <h4 style={{ color: "red", fontSize: '100px' }} >{post}</h4>
       </div>
 
+
+    
+{/* target은 해당 tag를 가리킴 */}
+{/* e.stopPropagation()은 이벤트 버블링을 막아주는것 */}
+    <div>
+    <input onChange={(e) => {setInput(e.target.value); console.log(e.target.value)}}></input>
+
+    {/* 타이틀 바꾸는 기능 */}
+    <button onClick={addd}>here</button>
+
+    {/* 글제목 바꾸는 기능 */}
+    <button onClick={() => {
+      let copy = [...logo];
+      try {
+      copy.unshift(input);
+      } catch (err){
+        alert(err.name); 
+      }
+      setLogo(copy)}}
+      >here 2</button>
+    
+    </div>
 {/*     
       <h4>{post}</h4>
       <div className="list">
@@ -37,6 +64,7 @@ function App() {
           // fuction 부분에는 두개의 파라미터가 들어감. 앞의 파라미터는 array의 순서대로 가져오는 거고
           // 뒤의 파라미터는 1씩 더해지는 정수임 그래서 h4부분에 a를 logo[i]로해도 i부분이 1씩 더해지기 때문에 array부분이 하나씩 나옴
           //map앞의 array 개수만큼 map이 돌아감 -> return 문 안에 있는 내용을 array에 담아줌 ex) [<div>hey</div>] -> 이런 형식을 react가 알아서 배열 벗겨서 실행하는거
+          
           logo.map(function(a,i){
             return (<div className="list">
             <h4 > {a} <button onClick={() => {
@@ -45,6 +73,14 @@ function App() {
               setLogo(copy);
             }}>글 수정 </button> </h4>
             <h4> <button onClick={() => {setModal(1); setCnt(i)}}>여기</button></h4>
+
+              {/* 삭제버튼 */}
+            <h4> <button onClick={() => {
+              let copy = [...logo];
+              copy.splice(i,1)
+              setLogo(copy);
+              }}>delete</button> </h4>
+
             <h4> <span onClick={() => { 
                 let copy = [...like];
                 copy[i] = copy[i] + 1;
@@ -69,6 +105,7 @@ function App() {
         modal == true ? <Modal color={'skyblue'} logo={logo} cnt={cnt}/> : null
       }
     </div>
+    </>
   );
 }
 
