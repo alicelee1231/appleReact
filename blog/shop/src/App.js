6edd98a69cce3cc1266504d3e676f253f45854b6
alicelee1231 <saemmilee1231@gmail.com>
2024-05-03 17:49:00 +0900

    styled공부

--- blog/shop/src/App.js
+++ blog/shop/src/App.js
@@ -19,90 +19,91 @@ function App() {
   return (
    <div>
     <Routes>
-      <Route path='/about' element={<div>about page</div>}/>
-    </Routes>
-  
-      <Navbar bg='dark' variant="dark">
-            <Container>
-            <Navbar.Brand href='#nome'>쨈미의 위시리스트</Navbar.Brand>
-            {['Primary', 'Secondary', 'Success'].map(
-              (variant) => (
-            <DropdownButton 
-                  as={ButtonGroup}
-                  key={variant}
-                  id={`dropdown-variants-${variant}`}
-                  variant={variant.toLowerCase()}
-                  title={variant}
-                  size='sm'
-                  className='mt-2'
-                  data-bs-theme = 'dark'>
-              <Dropdown.Item eventKey="1" to='/detail'>Action</Dropdown.Item>
-              <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
-              <Dropdown.Item eventKey="3" active>
-               Active Item
-              </Dropdown.Item>
-               <Dropdown.Divider />
-              <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
-            </DropdownButton>
-        ),
-      )}
-          </Container>
-            <Dropdown as={ButtonGroup}>
-            <Dropdown.Toggle id="dropdown-custom-1">Pow! Zoom!</Dropdown.Toggle>
-            <Dropdown.Menu className="super-colors">
-              <Dropdown.Item eventKey="1">Action</Dropdown.Item>
-              <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
-              <Dropdown.Item eventKey="3" active>
-                Active Item
-              </Dropdown.Item>
-               <Dropdown.Divider />
-               <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>{' '}
-      </Navbar>
-      {/* navigate에 숫자가 이동하면 그 숫자만큼 앞으로 이동 요청, -1는 뒤로 한페이지 */}
-      <Nav.Link onClick={() => {navigate(-1)}}>home</Nav.Link>
-
-      <Nav.Link onClick={() => {navigate('/detail')}}>Detail</Nav.Link>
-
-      <div className='main-bg'> </div>
-      <div style={{marginTop:"10px"}}>
-      <Link to="/"  style={{marginRight:"30px"}}>홈</Link>
-      <Link to="/detail">상세페이지</Link>
-      </div>
-      <Routes>
-        <Route path='/'element={<div className='row'>
-              {shoes.map((a,i) => {
-                  return  <Product className='Appa' key={shoes[i].id} shoes={shoes[i]} i ={i}></Product>       
-                })
-              }
-        </div>}>
-        </Route>
+        <Route path='/about' element={<div>about page</div>}/>
     </Routes>
     
-    <Routes>
-      <Route path='/detail' element={<Detail key={realProducts.id} realProducts={realProducts}></Detail>}/>
-     {/* 404페이지 만드는 거, 만약 페이지 주소를 잘 못 쳤을 때 404로 연결하는 거 */}
-     <Route path='/about' element={<About/>}></Route>
-      <Route path='*' element={<div>없는페이지임 바부야</div>}></Route>
+      <Navbar bg='dark' variant="dark">
+         <Container>
+           <Navbar.Brand href='#nome'>쨈미의 위시리스트</Navbar.Brand>
+              {['Primary', 'Secondary', 'Success'].map(
+                (variant) => (
+              <DropdownButton 
+                    as={ButtonGroup}
+                    key={variant}
+                    id={`dropdown-variants-${variant}`}
+                    variant={variant.toLowerCase()}
+                    title={variant}
+                    size='sm'
+                    className='mt-2'
+                    data-bs-theme = 'dark'>
+                <Dropdown.Item eventKey="1" to='/detail'>Action</Dropdown.Item>
+                <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+                <Dropdown.Item eventKey="3" active>
+                Active Item
+                </Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+              </DropdownButton>
+            ),
+          )}
+          </Container>
+                <Dropdown as={ButtonGroup}>
+                <Dropdown.Toggle id="dropdown-custom-1">Pow! Zoom!</Dropdown.Toggle>
+                <Dropdown.Menu className="super-colors">
+                  <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+                  <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+                  <Dropdown.Item eventKey="3" active>
+                    Active Item
+                  </Dropdown.Item>
+                  <Dropdown.Divider />
+                  <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+                </Dropdown.Menu>
+               </Dropdown>{' '}
+          </Navbar>
 
-      <Route path='/about' element={<About/>}>
-        <Route path="member" element={<div>멤버임</div>}></Route>
-        <Route path="location" element={<div>위치정보임</div>}></Route>
-        {/* 이 방식은  <Route path='/about' element={<About/>}>,  <Route path='/about/member' element={<About/>}> 랑 똑같은 방식임
-        => nested route 그 대신 어디에 보여줄 지 작성해야함 */}
-      </Route>
+        {/* navigate에 숫자가 이동하면 그 숫자만큼 앞으로 이동 요청, -1는 뒤로 한페이지 */}
+          <Nav.Link onClick={() => {navigate(-1)}}>home</Nav.Link>
+          <Nav.Link onClick={() => {navigate('/detail')}}>Detail</Nav.Link>
 
-          <Route path='/event' element={<Event/>}>
-          <Route path="one" element ={<h2>첫 주문시 양배추즙 서비스</h2>}></Route>
-          <Route path = "two" element={<h4>생일기념 쿠폰 받기</h4>}></Route>
+            <div className='main-bg'> </div>
+            <div style={{marginTop:"10px"}}>
+            <Link to="/"  style={{marginRight:"30px"}}>홈</Link>
+            <Link to="/detail">상세페이지</Link>
+            </div>
+         <Routes>
+            <Route path='/'element={<div className='row'>
+                  {shoes.map((a,i) => {
+                      return  <Product className='Appa' key={shoes[i].id} shoes={shoes[i]} i ={i}></Product>       
+                    })
+                  }
+            </div>}>
+            </Route>
+         </Routes>
+      
+       <Routes>
+          <Route path='/detail/:id' element={<Detail shoes={shoes}></Detail>}/>
+          
+
+      {/* 404페이지 만드는 거, 만약 페이지 주소를 잘 못 쳤을 때 404로 연결하는 거 */}
+            <Route path='/about' element={<About/>}>
+            <Route path='*' element={<div>없는페이지임 바부야</div>}></Route>
           </Route>
 
-    </Routes>
-      <div > 
-      
-    </div>
-  </div>
+          <Route path='/about' element={<About/>}>
+            <Route path="member" element={<div>멤버임</div>}></Route>
+            <Route path="location" element={<div>위치정보임</div>}></Route>
+          {/* 이 방식은  <Route path='/about' element={<About/>}>,  <Route path='/about/member' element={<About/>}> 랑 똑같은 방식임
+          => nested route 그 대신 어디에 보여줄 지 작성해야함 */}
+          </Route>
+
+          <Route path='/event' element={<Event/>}>
+            <Route path="one" element ={<h2>첫 주문시 양배추즙 서비스</h2>}></Route>
+            <Route path = "two" element={<h4>생일기념 쿠폰 받기</h4>}></Route>
+          </Route>
+        </Routes>
+        <div >  
+      </div>
+   </div>
   );
 }
 
--- blog/shop/src/routes/Detail.js
+++ blog/shop/src/routes/Detail.js
@@ -1,19 +1,47 @@
+import { useParams } from "react-router-dom";
+import styled from 'styled-components';
+
+let YellowBtn = styled.button `
+  background : yellow;
+  color : black;
+  padding : 10px
+`
+let Box = styled.div`
+  background : grey;
+  padding : 20px;
+`
+
 function Detail(props){
+
+
+  let {id} = useParams();
+  let want = props.shoes.find((x) => x.id == id)
+  console.log(id);
+
+
+
     return (
       <div className="container">
+        <Box>
+        <YellowBtn>Button</YellowBtn>
+        </Box>
       <div className="row">
       <div className="col-md-6">
         <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
       </div>
+      
+
       <div className="col-md-6">
         <h4 className="pt-5"></h4>
-        <p>{props.realProducts[0].name}</p>
-        <p>{props.realProducts[0].price}</p>
+        <p>{want.title}</p>
+        <p>{want.price}원</p>
+        <p>{want.content}</p>
         <button className="btn btn-danger">주문하기</button> 
       </div>
     </div>
   </div>
     )
   }
+
 
   export default Detail;
