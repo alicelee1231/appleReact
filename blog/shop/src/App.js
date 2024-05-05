a8f71f65b9837a36bc03ef432dbfc198afa9a7df
alicelee1231 <saemmilee1231@gmail.com>
2024-05-05 21:51:25 +0900

    axios공부

--- blog/shop/src/App.js
+++ blog/shop/src/App.js
@@ -1,141 +1,190 @@
-/*eslint-diabled*/ 
+/*eslint-diabled*/
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import { Navbar,Container,DropdownButton,Dropdown,ButtonGroup, Nav } from 'react-bootstrap';
-import { useState } from 'react';
-import {data, products} from './data';
-import {Route, Routes, Link, useNavigate, Outlet} from 'react-router-dom';
-import Detail from './routes/Detail';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import {
+  Navbar,
+  Container,
+  DropdownButton,
+  Dropdown,
+  ButtonGroup,
+  Nav,
+} from "react-bootstrap";
+import { useState } from "react";
+import { data, products } from "./data";
+import { Route, Routes, Link, useNavigate, Outlet } from "react-router-dom";
+import Detail from "./routes/Detail";
+import axios from "axios";
 
 function App() {
-
-  let [shoes] = useState(data);
+  let [shoes, setShoes] = useState(data);
   let [realProducts] = useState(products);
 
   //use라고 되어있는건 hook임 -> 훅이란 유용한 것이 들어있는거
   let navigate = useNavigate(); //페이지 이동을 도와줌
 
   return (
-   <div>
-    <Routes>
-        <Route path='/about' element={<div>about page</div>}/>
-    </Routes>
-    
-      <Navbar bg='dark' variant="dark">
-         <Container>
-           <Navbar.Brand href='#nome'>쨈미의 위시리스트</Navbar.Brand>
-              {['Primary', 'Secondary', 'Success'].map(
-                (variant) => (
-              <DropdownButton 
-                    as={ButtonGroup}
-                    key={variant}
-                    id={`dropdown-variants-${variant}`}
-                    variant={variant.toLowerCase()}
-                    title={variant}
-                    size='sm'
-                    className='mt-2'
-                    data-bs-theme = 'dark'>
-                <Dropdown.Item eventKey="1" to='/detail'>Action</Dropdown.Item>
-                <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
-                <Dropdown.Item eventKey="3" active>
+    <div>
+      <Routes>
+        <Route path="/about" element={<div>about page</div>} />
+      </Routes>
+
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#nome">쨈미의 위시리스트</Navbar.Brand>
+          {["Primary", "Secondary", "Success"].map((variant) => (
+            <DropdownButton
+              as={ButtonGroup}
+              key={variant}
+              id={`dropdown-variants-${variant}`}
+              variant={variant.toLowerCase()}
+              title={variant}
+              size="sm"
+              className="mt-2"
+              data-bs-theme="dark"
+            >
+              <Dropdown.Item eventKey="1" to="/detail">
+                Action
+              </Dropdown.Item>
+              <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+              <Dropdown.Item eventKey="3" active>
                 Active Item
-                </Dropdown.Item>
-                <Dropdown.Divider />
-                <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
-              </DropdownButton>
-            ),
-          )}
-          </Container>
-                <Dropdown as={ButtonGroup}>
-                <Dropdown.Toggle id="dropdown-custom-1">Pow! Zoom!</Dropdown.Toggle>
-                <Dropdown.Menu className="super-colors">
-                  <Dropdown.Item eventKey="1">Action</Dropdown.Item>
-                  <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
-                  <Dropdown.Item eventKey="3" active>
-                    Active Item
-                  </Dropdown.Item>
-                  <Dropdown.Divider />
-                  <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
-                </Dropdown.Menu>
-               </Dropdown>{' '}
-          </Navbar>
+              </Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+            </DropdownButton>
+          ))}
+        </Container>
+        <Dropdown as={ButtonGroup}>
+          <Dropdown.Toggle id="dropdown-custom-1">Pow! Zoom!</Dropdown.Toggle>
+          <Dropdown.Menu className="super-colors">
+            <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+            <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+            <Dropdown.Item eventKey="3" active>
+              Active Item
+            </Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>{" "}
+      </Navbar>
 
-        {/* navigate에 숫자가 이동하면 그 숫자만큼 앞으로 이동 요청, -1는 뒤로 한페이지 */}
-          <Nav.Link onClick={() => {navigate(-1)}}>home</Nav.Link>
-          <Nav.Link onClick={() => {navigate('/detail')}}>Detail</Nav.Link>
+      {/* navigate에 숫자가 이동하면 그 숫자만큼 앞으로 이동 요청, -1는 뒤로 한페이지 */}
+      <Nav.Link
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        home
+      </Nav.Link>
+      <Nav.Link
+        onClick={() => {
+          navigate("/detail");
+        }}
+      >
+        Detail
+      </Nav.Link>
 
-            <div className='main-bg'> </div>
-            <div style={{marginTop:"10px"}}>
-            <Link to="/"  style={{marginRight:"30px"}}>홈</Link>
-            <Link to="/detail">상세페이지</Link>
+      <div className="main-bg"> </div>
+      <div style={{ marginTop: "10px" }}>
+        <Link to="/" style={{ marginRight: "30px" }}>
+          홈
+        </Link>
+        <Link to="/detail">상세페이지</Link>
+      </div>
+
+      {/* axios 쓰는 방법, url의 경우 서버단에 요청 */}
+      <button
+        onClick={() => {
+          axios
+            .get("https://codingapple1.github.io/shop/data2.json")
+            .then((data) => {
+              let copy = [...shoes, ...data.data];
+              setShoes(copy);
+              console.log(shoes);
+            })
+            .catch(() => {
+              console.log("실패 ㅅㄱ");
+            });
+        }}
+      >
+        여기!
+      </button>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="row">
+              {shoes.map((a, i) => {
+                return (
+                  <Product
+                    className="Appa"
+                    key={shoes[i].id}
+                    shoes={shoes[i]}
+                    i={i}
+                  ></Product>
+                );
+              })}
             </div>
-         <Routes>
-            <Route path='/'element={<div className='row'>
-                  {shoes.map((a,i) => {
-                      return  <Product className='Appa' key={shoes[i].id} shoes={shoes[i]} i ={i}></Product>       
-                    })
-                  }
-            </div>}>
-            </Route>
-         </Routes>
-      
-       <Routes>
-          <Route path='/detail/:id' element={<Detail shoes={shoes}></Detail>}/>
-          
+          }
+        ></Route>
+      </Routes>
 
-      {/* 404페이지 만드는 거, 만약 페이지 주소를 잘 못 쳤을 때 404로 연결하는 거 */}
-            <Route path='/about' element={<About/>}>
-            <Route path='*' element={<div>없는페이지임 바부야</div>}></Route>
-          </Route>
+      <Routes>
+        <Route path="/detail/:id" element={<Detail shoes={shoes}></Detail>} />
 
-          <Route path='/about' element={<About/>}>
-            <Route path="member" element={<div>멤버임</div>}></Route>
-            <Route path="location" element={<div>위치정보임</div>}></Route>
+        {/* 404페이지 만드는 거, 만약 페이지 주소를 잘 못 쳤을 때 404로 연결하는 거 */}
+        <Route path="/about" element={<About />}>
+          <Route path="*" element={<div>없는페이지임 바부야</div>}></Route>
+        </Route>
+
+        <Route path="/about" element={<About />}>
+          <Route path="member" element={<div>멤버임</div>}></Route>
+          <Route path="location" element={<div>위치정보임</div>}></Route>
           {/* 이 방식은  <Route path='/about' element={<About/>}>,  <Route path='/about/member' element={<About/>}> 랑 똑같은 방식임
           => nested route 그 대신 어디에 보여줄 지 작성해야함 */}
-          </Route>
+        </Route>
 
-          <Route path='/event' element={<Event/>}>
-            <Route path="one" element ={<h2>첫 주문시 양배추즙 서비스</h2>}></Route>
-            <Route path = "two" element={<h4>생일기념 쿠폰 받기</h4>}></Route>
-          </Route>
-        </Routes>
-        <div >  
-      </div>
-   </div>
+        <Route path="/event" element={<Event />}>
+          <Route
+            path="one"
+            element={<h2>첫 주문시 양배추즙 서비스</h2>}
+          ></Route>
+          <Route path="two" element={<h4>생일기념 쿠폰 받기</h4>}></Route>
+        </Route>
+      </Routes>
+      <div></div>
+    </div>
   );
 }
 
 function Event() {
-  return(
+  return (
     <div>
-    <h4>오늘의 이벤트</h4>
-    <Outlet></Outlet>
+      <h4>오늘의 이벤트</h4>
+      <Outlet></Outlet>
     </div>
-  )
+  );
 }
 
-function About(){
+function About() {
   return (
     <div>
       <h4>회사 정보</h4>
       {/* nested route안에 있는 라우트들을 어디에 보여줄 건지 정하는게 outlet */}
       <Outlet></Outlet>
     </div>
-  )
+  );
 }
 
-function Product(props){
-  return(
-    <div  className='col-md-4'>
-        <img alt='내꺼' src={'img/cap' + (props.i ) + '.jpg'}   width='50%'></img>
-              <h5>{props.shoes.title}</h5>
-              <p>{props.shoes.price}</p>
-            </div>
-  )
+function Product(props) {
+  return (
+    <div className="col-md-4">
+      <img alt="내꺼" src={"img/cap" + props.i + ".jpg"} width="50%"></img>
+      <h5>{props.shoes.title}</h5>
+      <p>{props.shoes.price}</p>
+    </div>
+  );
 }
-
-
 
 export default App;
--- blog/shop/src/routes/Detail.js
+++ blog/shop/src/routes/Detail.js
@@ -1,47 +1,107 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import styled from 'styled-components';
+import styled from "styled-components";
 
-let YellowBtn = styled.button `
-  background : yellow;
-  color : black;
-  padding : 10px
-`
+// 같은 컴포넌트일 때 아래와 같이 props로 전달해도 가능, bg로 전달할 사항 넣어주기
+let YellowBtn = styled.button`
+  background: ${(props) => props.bg};
+  color: ${(props) => (props.bg == "blue" ? "white" : "black")};
+  padding: 10px;
+`;
 let Box = styled.div`
-  background : grey;
-  padding : 20px;
-`
+  background: grey;
+  padding: 20px;
+`;
 
-function Detail(props){
+// 이전의 css component가 마음에 들었으면 파라미터로 가져와서 복사해서 쓸 수 있음
+let NewBtn = styled.button(YellowBtn);
 
+// ---------------------------------------------------------------------
 
-  let {id} = useParams();
-  let want = props.shoes.find((x) => x.id == id)
-  console.log(id);
+function Detail(props) {
+  let { id } = useParams();
+  let [alert, setAlert] = useState(true);
+  let want = props.shoes.find((x) => x.id == id);
+  let [count, setCount] = useState(0);
+  let [num, setNum] = useState(true);
 
+  const onChange = (e) => {
+    setNum(e.target.value);
+  };
 
+  const onClick = () => {
+    if (isNaN(num) == 1) {
+      console.error("Only numbers are allowed");
+    } else {
+      console.log(num);
+    }
+  };
 
-    return (
-      <div className="container">
-        <Box>
-        <YellowBtn>Button</YellowBtn>
-        </Box>
+  useEffect(() => {
+    // 변수를 선언하면 return으로 clean up할 수 있음
+    // 재 랜더링 안해도됨
+    let a = setTimeout(() => {
+      setAlert(false);
+      console.log(2);
+    }, 2000);
+    // return () => {
+    // useEffect가 실행되기 전에 return이 먼저 실행됨
+    // clearTimeout( a);
+    // }
+    return () => {
+      console.log(1);
+      clearTimeout(a);
+    };
+  }, []);
+
+  // useEffect(() => {}) 1. 재랜더링마다 코드 실행
+  // useEffect(() => {}, []) 2. mount시 1회 코드 실행
+  // useEffect(() => {
+  //   return() =>{    3. unmount시 1회 코드 실행
+
+  //   }  4. useEffect실행 전, 뭔가 실행하려면 언제나 return()=>{}에서 실행
+  // })
+
+  return (
+    <div className="container">
+      {alert === true ? (
+        <div className="alert alert-warning"> 2초 이내 구매시 할인</div>
+      ) : null}
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        {count}
+        button
+      </button>
+
+      <input onChange={onChange}></input>
+      <button onClick={onClick}>버튼이다</button>
+      {!num >= 48 && !num <= 57(<div>try again</div>)}
+
+      <Box>
+        <YellowBtn bg="yellow">Button</YellowBtn>
+        <YellowBtn bg="blue">Button1</YellowBtn>
+      </Box>
       <div className="row">
-      <div className="col-md-6">
-        <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-      </div>
-      
+        <div className="col-md-6">
+          <img
+            src="https://codingapple1.github.io/shop/shoes1.jpg"
+            width="100%"
+          />
+        </div>
 
-      <div className="col-md-6">
-        <h4 className="pt-5"></h4>
-        <p>{want.title}</p>
-        <p>{want.price}원</p>
-        <p>{want.content}</p>
-        <button className="btn btn-danger">주문하기</button> 
+        <div className="col-md-6">
+          <h4 className="pt-5"></h4>
+          <p>{want.title}</p>
+          <p>{want.price}원</p>
+          <p>{want.content}</p>
+          <button className="btn btn-danger">주문하기</button>
+        </div>
       </div>
     </div>
-  </div>
-    )
-  }
+  );
+}
 
-
-  export default Detail;
+export default Detail;
